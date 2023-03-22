62071c245129874e3cd9e3738a4f96d55e0e6069
M‎ᵃ Fernanda Herrera García <109182712+MariaHerrera03@users.noreply.github.com>
2023-03-21 22:51:14 -0500

    Update index.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://programate-team-back-bjvyzja12-manuel101284.vercel.app/"
 });
